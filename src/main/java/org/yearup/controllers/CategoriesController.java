3f3398945979162971375019eb4a4547cdef6131
jpress1409 <jpress1409@gmail.com>
2024-12-13 11:21:34 -0500

    Filled in all methods in CategoriesController. Still need Auth

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,6 +1,7 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
@@ -23,46 +24,45 @@ public class CategoriesController
     public CategoriesController(CategoryDao categoryDao){
         this.categoryDao = categoryDao;
     }
+    @Autowired
+    public CategoriesController(ProductDao productDao){
+        this.productDao = productDao;
+    }
+
 
     @RequestMapping(path = "/categories", method = RequestMethod.GET)
     public List<Category> getAll() {
         return categoryDao.getAllCategories();
     }
 
-    // add the appropriate annotation for a get action
+    @RequestMapping(path = "/categories/{id}", method = RequestMethod.GET)
     public Category getById(@PathVariable int id) {
         return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
-        // get a list of product by categoryId
-        return null;
+    @GetMapping("/category/{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+        return productDao.listByCategoryId(categoryId);
     }
 
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
-    public Category addCategory(@RequestBody Category category)
-    {
-        // insert the category
-        return null;
+    @RequestMapping(path = "/category", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Category addCategory(@RequestBody Category category) {
+        return categoryDao.create(category);
     }
 
-    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    @RequestMapping(path = "/category/{id}", method = RequestMethod.PUT)
     // add annotation to ensure that only an ADMIN can call this function
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
-        // update the category by id
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
+        categoryDao.update(id, category);
     }
 
 
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    @RequestMapping(path = "/category/{id}", method = RequestMethod.DELETE)
     // add annotation to ensure that only an ADMIN can call this function
-    public void deleteCategory(@PathVariable int id)
-    {
-        // delete the category by id
+    public void deleteCategory(@PathVariable int id) {
+        categoryDao.delete(id);
     }
 }
